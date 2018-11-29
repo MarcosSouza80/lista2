6de382071a9a5562062eeabd6b3ca49db5431291
Marcos Souza <marcosrg754@gmail.com>
2018-11-29 09:40:48 -0200

    primeiro commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Lista de Compras</title>
+    </head>
+    <body>
+        <?php
+         require_once 'menu.php';
+         ?>
+      <br><br><br><br>
+        <h1 align="center"> <i>  Bem-vindo </i> </h1>
+        
+    </body>
+</html>
